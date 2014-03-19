cb02df5653edb10213c4b880794e1b5939c604b7
Emmanuel Fonseca <efonseca@phas.ubc.ca>
2014-03-19 02:38:30 -0700

    added bias-removal option

--- dm.py
+++ dm.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 
-def calcstruct(inf,nbinsmax=20):
+def calcstruct(inf,nbinsmax=20,rmbias=False):
   """
   Calculate a structure funtion from DMX measurements.
 
@@ -12,7 +12,8 @@ def calcstruct(inf,nbinsmax=20):
     - 'inf' = input DMX file (obtained from running 'dmxparse' on a best-fit parfile).
 
   Default arguments:
-    - 'nbinsmax' = maximum number of time-lag bins.
+    - 'nbinsmax' = maximum number of time-lag bins. (default is 20)
+    - 'rmbias' = remove bias term? (default is no/False)
   """
   # read in data, declare arrays.
   mjd, dmx, err = [], [], []
@@ -29,6 +30,7 @@ def calcstruct(inf,nbinsmax=20):
 
   dmxsq    = []  # squared difference in DMX.
   dmxsqerr = []  # error of squared difference.
+  rmserr   = []  # sum of squares of DMX errors.
   lags     = []  # time difference ('lag') between DMX pair.
 
   # calculate all unique squared difference in DMs.
@@ -37,10 +39,12 @@ def calcstruct(inf,nbinsmax=20):
     for j in range(len(dmb1)):
       dmxsq.append((dmx[i]-dmb1[j])**2)
       dmxsqerr.append(2.*np.sqrt(((dmx[i]-dmb1[j])**2)*(err[i]**2+dmb3[j]**2)))
+      rmserr.append((err[i])**2+(dmb3[j])**2)
       lags.append(np.fabs(mjd[i]-dmb2[j]))
 
   dmxsq    = np.array(dmxsq)
   dmxsqerr = np.array(dmxsqerr)
+  rmserr   = np.array(rmserr)
   lags     = np.array(lags)
 
   # print some info.
@@ -53,19 +57,19 @@ def calcstruct(inf,nbinsmax=20):
   binedge = 10.**(np.linspace(np.log10(minlag-1.),np.log10(maxlag+1.),num=(nbinsmax+1)))
   bins, struct, structerr = [], [], []
 
-  nbincount = 0
   # compute structure function!
   for k in range(nbinsmax):
     ind = np.where((lags >= binedge[k]) & (lags < binedge[k+1]))
+    structbias    = 0.
+    structbiaserr = 0.
     # if bin is not empty, proceed.
     if ((dmxsq[ind]).size != 0):
-      nbincount += len(dmxsq[ind])
-      structmean     = np.mean(dmxsq[ind]) 
-      #if (structmean > 0.):
-      struct.append(np.mean(dmxsq[ind]))
+      if (rmbias):
+        structbias    = np.mean(rmserr[ind])
+        structbiaserr = rmserr[ind]
+      struct.append(np.mean(dmxsq[ind])-structbias)
       bins.append(np.mean([binedge[k],binedge[k+1]]))
-      #structerr.append(np.std(dmxsq[ind])/np.sqrt((dmxsq[ind]).size))
-      structerr.append(np.sqrt(np.sum((dmxsqerr[ind])**2))/np.float(dmxsq[ind].size))
+      structerr.append(np.sqrt(np.sum((dmxsqerr[ind])**2+structbiaserr**2))/np.float(dmxsq[ind].size))
 
   bins       = np.array(bins) 
   struct     = ((2.*np.pi/1400e6/2.41e-16)**2)*np.array(struct)
