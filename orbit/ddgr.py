31c242d7ccdf859596cab69092c390d3e0a60dc3
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2017-02-28 14:29:24 -0500

    added some missing DDGR expressions.

--- orbit/ddgr.py
+++ orbit/ddgr.py
@@ -1,45 +1,57 @@
 #! /usr/bin/python
 
-import astropy.constants as const
+from PSRpy.const import c, G, M_sun, T_sun
 import numpy as np
 
-# define some constants.
-c    = const.c.value
-G    = const.G.value
-Msun = const.M_sun.value
-Tsun = G*Msun/c**3
 pi   = np.pi
 
-def pbdot_GR(m1,m2,pb,e):
+def gamma_GR(m1, m2, pb, e):
     """
-    Calculate orbital decay, as expected from GR.
+    Calculates time-averaged gravitational redshift / time dilation parameter, 
+    as expected from GR.
     """
-    # declare constant (i.e.non-mass) terms.
-    pb = np.float(pb)*86400.
-    e  = np.float(e)
-    fe = (1.+(73./24.*e**2)+(37./96.*e**4))*(1.-e**2)**(-3.5)
-    A  = -192.*pi/5.*(pb/2./pi)**(-5./3.)*fe*Tsun**(5./3.)
-    # calculate!
+    
+    pb_in = pb * 86400
+    A = e * (pb_in / 2 / pi)**(1./3.) * T_sun**(2./3.)
+    return A * m2 * (m1 + 2 * m2) / (m1 + m2)**(4./3.)
+
+def omdot_GR(m1, m2, pb, e):
+    """
+    Calculates periastron advance, as expected from GR.
+    """
+
+    pb_in = pb * 86400
+    omdot = 3 * (pb_in / 2 / pi)**(-5./3.) * (T_sun * (m1 + m2))**(2./3.) / (1 - e**2)
+    return omdot * 180 / pi * 86400 * 365.25
+
+def pbdot_GR(m1, m2, pb, e):
+    """
+    Calculates orbital decay of binary period, as expected from GR.
+    """
+
+    pb_in = np.float(pb)*86400.
+    fe = (1 + (73. / 24. * e**2) + (37. / 96. * e**4)) * (1 - e**2)**(-3.5)
+    A  = -192 * pi / 5 * (pb_in / 2 / pi)**(-5./3.) * fe * T_sun**(5./3.)
     return A*m1*m2*(m1+m2)**(-1./3.)
 
 def xdot_GR(m1, m2, pb, e):
     """
-    Calculate rate of orbital decay for semi-major axis.
+    Calculates rate of orbital decay for semi-major axis.
     Note: XDOT_GR = this rate x SINI.
     """
-    # compute non-mass terms.
+
+    pb_in = pb * 86400
     fe = (1 + (73./24.) * e**2 + (37./96.) * e**4) / (1 - e**2)**(7./2.)
-    A = -64. / 5. * (2 * np.pi * Tsun / pb)**2 * fe
-    # calculate!
+    A = -64. / 5. * (2 * pi * T_sun / pb_in)**2 * fe
     return A * m1 * m2**2 / (m1 + m2)
 
 def edot_GR(m1, m2, pb, e):
     """
-    Calculate rate of circularization from GR.
+    Calculates rate of circularization from GR.
     """
     nb = 2 * np.pi / pb
     eterm = (1 + (121./304.) * e**2) / (1 - e**2)**(5./2.)
-    A = -304. / 15. * Tsun**(5./3.) * nb**(8./3.) * eterm
+    A = -304. / 15. * T_sun**(5./3.) * nb**(8./3.) * eterm
     return A * m1 * m2 / (m1 + m2)**(1./3.) * e
 
 def dtheta_GR(m1, m2, pb, e):
@@ -47,14 +59,14 @@ def dtheta_GR(m1, m2, pb, e):
     Calculate the dtheta orbital-shape correction due to GR.
     """
     nb = 2 * np.pi / pb
-    A = (Tsun * nb)**(2./3.)
+    A = (T_sun * nb)**(2./3.)
     return A * (7. * m1**2 / 2. + 6 * m1 * m2 + 2 * m2**2) / (m1 + m2)**(4./3.)
 
 def precession_GR(m1, m2, pb, e):
     """
     Calculate rate of geodetic precession of the spin axis.
     """
-    A = (2 * np.pi / pb)**(5./3.) * Tsun**(2./3.) / (1 - e**2)
+    A = (2 * np.pi / pb)**(5./3.) * T_sun**(2./3.) / (1 - e**2)
     return A * m2 * (4 * m1 + 3 * m2) / 2 / (m1 + m2)**(4./3.)
 
 # compute terms due to rotational aberration.
