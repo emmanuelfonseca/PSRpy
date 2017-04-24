a5173e21c16dc4513dc1d099914e9a2c2ee9b79f
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2017-04-24 17:53:34 -0400

    commented out pywt stuff, temporarily.

--- profile/profutils.py
+++ profile/profutils.py
@@ -3,90 +3,90 @@
 import numpy as np
 import pywt
 
-def smooth_wavelet1D(indat, wave='sym8', nlevel=5, ncycle=10, threshtype='hard'):
-    """
-    Compute the wavelet-denoised version of an input profile/waveform. 
-    
-    Required arguments: 
-        - 'indat' = input data in 1-dimensional array. 
+#def smooth_wavelet1D(indat, wave='sym8', nlevel=5, ncycle=10, threshtype='hard'):
+#    """
+#    Compute the wavelet-denoised version of an input profile/waveform. 
+#    
+#    Required arguments: 
+#        - 'indat' = input data in 1-dimensional array. 
+#
+#    Default arguments: 
+#        - 'wave' = mother wavelet (default 'sym8') 
+#        - 'nlevel' = number of decomposition levels (default '5') 
+#        - 'ncycle' = number of circulant averages to compute (default '10') 
+#        - 'threshtype' = type of wavelet thresholding (default 'hard')
+#    """
 
-    Default arguments: 
-        - 'wave' = mother wavelet (default 'sym8') 
-        - 'nlevel' = number of decomposition levels (default '5') 
-        - 'ncycle' = number of circulant averages to compute (default '10') 
-        - 'threshtype' = type of wavelet thresholding (default 'hard')
-    """
+#    nbins = len(indat)
+#    data  = np.zeros(nbins)
 
-    nbins = len(indat)
-    data  = np.zeros(nbins)
-
-    # carry out translation-invariant wavelet denoising.
-    for j in range(ncycle):
-        m = j - ncycle/2 - 1
-        coeffs = pywt.wavedec(np.roll(indat,m),wave,level=nlevel)
-        # get threshold value.
-        lopt = np.median(np.fabs(coeffs[1]))/0.6745*np.sqrt(2.*np.log(nbins))
-        # now do wavelet thresholding.
-        for k in range(1,nlevel+1):
-        # hard threshold.
-            if (threshtype == 'hard'):
-                (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
-        # soft threshold. 
-        else:
-            (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
-            (coeffs[k])[np.where(coeffs[k] > lopt)] = (coeffs[k])[np.where(coeffs[k] > lopt)]+lopt
-            (coeffs[k])[np.where(coeffs[k] < -lopt)] = (coeffs[k])[np.where(coeffs[k] < -lopt)]-lopt
-        # reconstruct data.
-        data = data + np.roll(pywt.waverec(coeffs,wave),-m)   
+#    # carry out translation-invariant wavelet denoising.
+#    for j in range(ncycle):
+#        m = j - ncycle/2 - 1
+#        coeffs = pywt.wavedec(np.roll(indat,m),wave,level=nlevel)
+#        # get threshold value.
+#        lopt = np.median(np.fabs(coeffs[1]))/0.6745*np.sqrt(2.*np.log(nbins))
+#        # now do wavelet thresholding.
+#        for k in range(1,nlevel+1):
+#        # hard threshold.
+#            if (threshtype == 'hard'):
+#                (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
+#        # soft threshold. 
+#        else:
+#            (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
+#            (coeffs[k])[np.where(coeffs[k] > lopt)] = (coeffs[k])[np.where(coeffs[k] > lopt)]+lopt
+#            (coeffs[k])[np.where(coeffs[k] < -lopt)] = (coeffs[k])[np.where(coeffs[k] < -lopt)]-lopt
+#        # reconstruct data.
+#        data = data + np.roll(pywt.waverec(coeffs,wave),-m)   
   
-    return data/np.float(ncycle)
+#    return data/np.float(ncycle)
 
-def smooth_wavelet2D(indat, wave='sym8', nlevel=5, ncycle=10, threshtype='hard'):
-    """
-    Compute the wavelet-denoised version of a set of profiles/waveforms.
+#def smooth_wavelet2D(indat, wave='sym8', nlevel=5, ncycle=10, threshtype='hard'):
+#    """
+#    Compute the wavelet-denoised version of a set of profiles/waveforms.
+#  
+#    Required argument:
+#        - 'indat' = (nbins x nchans) data array.
+#  
+#    Default arguments: 
+#        - 'wave' = mother wavelet (default 'sym8') 
+#        - 'nlevel' = number of decomposition levels (default '5') 
+#        - 'ncycle' = number of circulant averages to compute (default '10') 
+#        - 'threshtype' = type of wavelet thresholding (default 'hard')
+#    """
+
+#    nbins = len(indat[:,0])
+#    nchan = len(indat[0,:])
+
+#    outdat = np.zeros((nbins,nchan))
   
-    Required argument:
-        - 'indat' = (nbins x nchans) data array.
-  
-    Default arguments: 
-        - 'wave' = mother wavelet (default 'sym8') 
-        - 'nlevel' = number of decomposition levels (default '5') 
-        - 'ncycle' = number of circulant averages to compute (default '10') 
-        - 'threshtype' = type of wavelet thresholding (default 'hard')
-    """
-
-    nbins = len(indat[:,0])
-    nchan = len(indat[0,:])
-
-    outdat = np.zeros((nbins,nchan))
-  
-    # smooth each channel.
-    for i in range(nchan):
-        prof = indat[:,i]
-        data = 0.
-        # carry out translation-invariant wavelet denoising.
-        for j in range(ncycle):
-            m = j - ncycle/2 - 1
-            coeffs = pywt.wavedec(np.roll(prof,m),wave,level=nlevel)
-            # get threshold value.
-            lopt = np.median(np.fabs(coeffs[1]))/0.6745*np.sqrt(2.*np.log(nbins))
-            # now do wavelet thresholding.
-            for k in range(1,nlevel+1):
-                # hard threshold.
-                if (threshtype == 'hard'):
-                    (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
-                # or soft threshold.
-                else:
-                    (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
-                    (coeffs[k])[np.where(coeffs[k] > lopt)] = (coeffs[k])[np.where(coeffs[k] > lopt)]+lopt
-                    (coeffs[k])[np.where(coeffs[k] < -lopt)] = (coeffs[k])[np.where(coeffs[k] < -lopt)]-lopt
-            # reconstruct data.
-            data = data + np.roll(pywt.waverec(coeffs,wave),-m)
-        # save averaged profile.
-        outdat[:,i] = data/np.float(ncycle)
-
-    # return smoothed portrait.
-    return outdat
+#    # smooth each channel.
+#    for i in range(nchan):
+#        prof = indat[:,i]
+#        data = 0.
+#        # carry out translation-invariant wavelet denoising.
+#        for j in range(ncycle):
+#            m = j - ncycle/2 - 1
+#            coeffs = pywt.wavedec(np.roll(prof,m),wave,level=nlevel)
+#            # get threshold value.
+#            lopt = np.median(np.fabs(coeffs[1]))/0.6745*np.sqrt(2.*np.log(nbins))
+#            # now do wavelet thresholding.
+#            for k in range(1,nlevel+1):
+#                # hard threshold.
+#                if (threshtype == 'hard'):
+#                    (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
+#                # or soft threshold.
+#                else:
+#                    (coeffs[k])[np.where(np.fabs(coeffs[k]) < lopt)] = 0.
+#                    (coeffs[k])[np.where(coeffs[k] > lopt)] = (coeffs[k])[np.where(coeffs[k] > lopt)]+lopt
+#                    (coeffs[k])[np.where(coeffs[k] < -lopt)] = (coeffs[k])[np.where(coeffs[k] < -lopt)]-lopt
+#            # reconstruct data.
+#            data = data + np.roll(pywt.waverec(coeffs,wave),-m)
+#        # save averaged profile.
+#        outdat[:,i] = data/np.float(ncycle)
+#
+#    # return smoothed portrait.
+#    return outdat
 
 def smooth_pca(indat, ncomp=5):
     """
