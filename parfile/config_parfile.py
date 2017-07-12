4a45b11a918f55df82f2f5964ea03b779e698a64
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2017-07-11 22:37:26 -0400

    added second-orbit parameters to config list.

--- parfile/config_parfile.py
+++ parfile/config_parfile.py
@@ -3,7 +3,8 @@ parameter_list = ["PMRA", "PMDEC", "LAMBDA", "BETA", "PMLAMBDA",
                   "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", 
                   "F17", "F18", "F19", "F20", "DM", "DMX", "FD1", "FD2", "FD3", "FD4", 
                   "FD5", "DM", "DMX", "A1", "E", "T0", "PB", "OM", "OMDOT", "GAMMA", "PBDOT", 
-                  "XPBDOT", "SINI", "MTOT", "M2", "XDOT", "DTHETA", "DR", "JUMP"]
+                  "XPBDOT", "SINI", "MTOT", "M2", "XDOT", "DTHETA", "DR", "JUMP", 
+                  "A1_2", "E_2", "PB_2", "OM_2", "T0_2"]
 
 spin_freq_derivatives = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", 
                          "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20"]
