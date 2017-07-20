8aa1d45f1885ad646dfafdbd341508bab3d7dcf9
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2017-07-20 15:15:11 -0400

    moved JUMP to error-parameter list.

--- parfile/config_parfile.py
+++ parfile/config_parfile.py
@@ -3,7 +3,7 @@ parameter_list = ["PMRA", "PMDEC", "LAMBDA", "BETA", "PMLAMBDA",
                   "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", 
                   "F17", "F18", "F19", "F20", "DM", "DMX", "FD1", "FD2", "FD3", "FD4", 
                   "FD5", "DM", "DMX", "A1", "E", "T0", "PB", "OM", "OMDOT", "GAMMA", "PBDOT", 
-                  "XPBDOT", "SINI", "MTOT", "M2", "XDOT", "DTHETA", "DR", "JUMP", 
+                  "XPBDOT", "SINI", "MTOT", "M2", "XDOT", "DTHETA", "DR",  
                   "A1_2", "E_2", "PB_2", "OM_2", "T0_2", "PEPOCH"]
 
 spin_freq_derivatives = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", 
@@ -16,4 +16,4 @@ string_list = ["PSR", "RAJ", "DECJ", "START", "FINISH", "SOLARN0", "EPHEM", "ECL
 
 int_list    = ["NTOA", "NITS", "NDDM", "EPHVER", "MODE"]
 
-error_list = ["T2EFAC", "T2EQUAD", "ECORR"]
+error_list = ["T2EFAC", "T2EQUAD", "ECORR", "JUMP"]
