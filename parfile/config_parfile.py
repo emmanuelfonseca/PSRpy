17188cae1d9f042b2442a3f0f8866688d9d05109
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2017-07-24 15:56:12 -0400

    moved START/FINISH to fit params.

--- parfile/config_parfile.py
+++ parfile/config_parfile.py
@@ -4,12 +4,12 @@ parameter_list = ["PMRA", "PMDEC", "LAMBDA", "BETA", "PMLAMBDA",
                   "F17", "F18", "F19", "F20", "DM", "DMX", "FD1", "FD2", "FD3", "FD4", 
                   "FD5", "DM", "DMX", "A1", "E", "T0", "PB", "OM", "OMDOT", "GAMMA", "PBDOT", 
                   "XPBDOT", "SINI", "MTOT", "M2", "XDOT", "DTHETA", "DR",  
-                  "A1_2", "E_2", "PB_2", "OM_2", "T0_2", "PEPOCH"]
+                  "A1_2", "E_2", "PB_2", "OM_2", "T0_2", "PEPOCH", "START", "FINISH"]
 
 spin_freq_derivatives = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", 
                          "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20"]
 
-string_list = ["PSR", "RAJ", "DECJ", "START", "FINISH", "SOLARN0", "EPHEM", "ECL", "CLK",
+string_list = ["PSR", "RAJ", "DECJ", "SOLARN0", "EPHEM", "ECL", "CLK",
                "UNITS", "TIMEEPH", "T2CMETHOD", "CORRECT_TROPOSPHERE", "PLANET_SHAPIRO",
                "DILATEFREQ", "NTOA", "TRES", "TZRMJD", "TZRFRQ", "TZRSITE", "NITS", "INFO",
                "BINARY", "DCOVFILE"]
