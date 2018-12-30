8d4b9a9cc78a073a97ac95e9f92fbfbaf3a75edc
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2018-12-29 21:06:52 -0500

    created one Roemer-delay function to accommodate different models.

--- orbit/timedelays.py
+++ orbit/timedelays.py
@@ -4,10 +4,9 @@ from PSRpy.const import au, c, d2r, pc, T_sun
 import orbvectors as ov
 import ssbfuncs as ssb
 import orbfuncs as o
-import mpmath as mp
 import numpy as np
-
-mp.mp.prec = 80
+import ddgr 
+import sys
 
 def roemer_delay_ssb(epoch, ecl_b, ecl_l):
     """
@@ -158,89 +157,124 @@ def annual_orbital_parallax_delay(x, pb, ecc, om, t0, epoch, incl, asc, ecl_b, e
     else:
         return np.sum(np.cross(r_earth, s) * np.cross(r_pulsar, s)) / 2 / c / (d * 1000 * pc)
 
-def pulsar_roemer_delay_BT(dates, x, pb, ecc, om, t0, xdot=0, pbdot=0, omdot=0, gamma=0, 
-    tolerance=1e-12, mp_version=False):
+def pulsar_roemer_delay(dates, orbital_elements, xdot=0, pbdot=0, omdot=0, gamma=0, 
+    eps1dot=0, eps2dot=0, m1=0, m2=0, dtheta=0, tolerance=1e-12, binary_model="DD", 
+    orbital_phase=False):
     """
     Computes the Roemer timing delay for a pulsar in an eccentric binary system, given the 
-    orbital elements and date(s). The timing formula is taken from Blandford & Teukolsky 
-    (ApJ, 1976, 205, 580).
+    orbital elements and date(s). 
 
     Parameters
     ----------
 
     dates : float 
         epoch(s) to evaluate time delay, in units of MJD
-    x : float
-        projected semimajor axis of pulsar orbit, in units of light-seconds.
-    pb : float
-        orbital period, in units of days.
-    ecc : float 
-        orbital eccentricity, with 0 <= ecc < 1 for bound orbits.
-    om : float 
-        argument of periastron, in units of degrees.
-    t0 : float 
-        epoch of periastron passage, in units of MJD
+    orbital_elements : tuple of floats
+        the five orbital elements, in the following order for each listed binary model:
+            - DD   : (x, pb, ecc, om, t0)
+            - BT   : (x, pb, ecc, om, t0)
+            - DDGR : (x, pb, ecc, om, t0)
+            - ELL1 : (x, pb, eps1, eps2, tasc)
+        a description of the elements is provded in the Notes section below
     xdot : float, optional
         rate of change in x, in units of 1e-12.
     pbdot : float, optional
         rate of change in pb, in units of 1e-12
     omdot : float, optional
-        rate of change in periastron argument, in units of degree per year.
+        rate of change in om, in units of degree per year.
     gamma : float, optional 
         parameter for time dilation / gravitational redshift, in units of seconds.
+    m1 : float, optional
+        pulsar mass, in units of solar masses (used by 'DDGR', if set)
+    m2 : float, optional
+        companion mass, in units of solar masses (used by 'DDGR', if set)
+    dtheta : float
+        deformation of eccentricity in angular equation of motion
+    tolerance : float, optional
+        tolerance used to Newton-Raphson evaluation of eccentric anomaly 
+        (default value is 1e-12)
+    binary_model : {'DD', 'BT', 'DDGR','ELL1'}
+        short name for binary model to use in calculating the Roemer delay
 
     Returns
     -------
     
     float
         Roemer time delay due to orbital motion, in seconds.
+
+    Notes
+    -----
+    
+    x   : projected semimajor axis of pulsar orbit, in units of light-seconds.
+    pb  : orbital period, in units of days.
+    ecc : orbital eccentricity, with 0 <= ecc < 1 for bound orbits.
+    om  : argument of periastron, in units of degrees.
+    t0  : epoch of periastron passage, in units of MJD
+
+    The timing formula for each model can be found as follows:
+        - BT: Blandford & Teukolsky (ApJ, 1976, 205, 580)
+        - DD:
+        - ELL1:
     """
 
-    if mp_version:
-        
-        if isinstance(dates, list):
-            roemer_delay = []
+    eccentric_orbit_models = ["BT", "DD", "DDGR"]
 
-            for ii in range(len(dates)):
-                print ii
-                ma = o.mean_anomaly(pb, dates[ii], t0, pbdot=pbdot, mp_version=mp_version) 
-                ea = o.ecc_anomaly(ma, ecc, tolerance=tolerance, mp_version=mp_version) 
-                ta = o.true_anomaly(ea, ecc, mp_version=mp_version) 
-                om = o.peri_omega(om, pb, ta, omdot=omdot) 
-                se, ce = mp.sin(ea * mp.pi / mp.mpf('180')), mp.cos(ea * mp.pi / mp.mpf('180'))
-                so, co = mp.sin(om * mp.pi / mp.mpf('180')), mp.cos(om * mp.pi / mp.mpf('180'))
-                roemer_delay.append(x * (ce - ecc) * so + x * se * np.sqrt(1 - ecc**2) * co)
-
-            return roemer_delay
-
-        else:
-
-            ma = o.mean_anomaly(pb, dates, t0, pbdot=pbdot, mp_version=mp_version) 
-            ea = o.ecc_anomaly(ma, ecc, tolerance=tolerance, mp_version=mp_version) 
-            ta = o.true_anomaly(ea, ecc, mp_version=mp_version) 
-            om = o.peri_omega(om, pb, ta, omdot=omdot, mp_version=mp_version) 
-            se, ce = mp.sin(ea * mp.pi / mp.mpf('180')), mp.cos(ea * mp.pi / mp.mpf('180'))
-            so, co = mp.sin(om * mp.pi / mp.mpf('180')), mp.cos(om * mp.pi / mp.mpf('180'))    
-
-            return x * (ce - ecc) * so + x * se * mp.sqrt(mp.mpf('1') - mp.power(ecc, 2)) * co
-
-    else:
+    if (binary_model in eccentric_orbit_models):
+        x, pb, ecc, om, t0 = orbital_elements
 
         # first, compute Keplerian term.
-        ma = o.mean_anomaly(pb, dates, t0, pbdot=pbdot) 
+        ma = o.mean_anomaly(pb, dates, t0, pbdot=(pbdot * 1e-12)) 
         ea = o.ecc_anomaly(ma, ecc, tolerance=tolerance) 
         ta = o.true_anomaly(ea, ecc) 
-        om = o.peri_omega(om, pb, ta, omdot=omdot) 
+        om = o.periastron_argument(om, pb, dates, ta, t0, omdot=omdot, binary_model=binary_model) 
         se, ce = np.sin(ea * d2r), np.cos(ea * d2r)
         so, co = np.sin(om * d2r), np.cos(om * d2r)
-        alpha = x * so
-        beta = np.sqrt(1 - ecc**2) * x * co
+        alpha = (x + (xdot * 1e-12) * (dates - t0)) * so
+        beta = np.sqrt(1 - ecc**2) * (x + (xdot * 1e-12) * (dates - t0)) * co
         delay = alpha * (ce - ecc) + (beta + gamma) * se
 
         # next, compute second-order correction term.
-        delay += (alpha * se - beta * ce) * delay / (pb / 2 / np.pi) / (1 - ecc * ce)
+
+        if (binary_model == "BT"):
+            delay += (alpha * se - beta * ce) * delay / ((pb * 86400) / 2 / np.pi) / (1 - ecc * ce)
+        elif (binary_model == "Keplerian"):
+            # for purely Keplerian motion, remove gamma term if supplied gamma is non-zero.
+            delay -= gamma * se
 
         return delay
+
+    elif (binary_model == "ELL1"):
+        x, pb, eps1, eps2, tasc = orbital_elements
+
+        # compute orbital elements and their variations from supplied derivatives.
+        nb = 2 * np.pi / pb
+        nbdot = -2 * np.pi / pb**2 * (pbdot * 1e-12)
+        xnew = x + (xdot * 1e-12) * (dates - tasc)
+        eps1new = eps1 + (eps1dot * 1e-12) * (dates - tasc)
+        eps2new = eps2 + (eps2dot * 1e-12) * (dates - tasc)
+
+        # determine OMDOT and EDOT from derivatives and values of the EPS1/EPS2 parameters.
+        om = np.arctan2(eps1, eps2) * 180 / np.pi + 180
+        so, co = np.sin(om * np.pi / 180), np.cos(om * np.pi / 180)
+        ecc = np.sqrt(eps1**2 + eps2**2)
+        a = np.array([[so, ecc * co], [co, -ecc * so]])
+        b = np.array([eps1dot * 1e-12, eps2dot * 1e-12])
+        edot, omdot = np.linalg.solve(a, b)
+
+        # compute eccentricity terms for proper calculation of effective orbital frequency.
+        t0 = tasc + (om * np.pi / 180) / (nb + (omdot * np.pi / 180 / 365.25 / 86400))
+        nbbar = nb + (omdot * np.pi / 180 / 365.25 / 86400) - nbdot * (t0 - tasc)
+        phi = (nbbar * (dates - tasc) + 0.5 * nbdot * (dates - tasc)**2) % (2 * np.pi)
+
+        # compute delay.
+        delay = xnew * (np.sin(phi) + 0.5 * eps2 * np.sin(2 * phi) - 0.5 * eps1 * np.cos(2 * phi))
+
+        if orbital_phase:
+            return (phi, delay)
+        
+        else:
+            return delay
+
 
 def pulsar_roemer_delay_ELL1(dates, x, pb, eps1, eps2, tasc, xdot=0, pbdot=0, eps1dot=0, 
     eps2dot=0, omdot=0, om=0):
