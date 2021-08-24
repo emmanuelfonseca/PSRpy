5ba26bc517478d5b6d2f3b14eb6f9210acf916b0
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2021-08-24 14:02:17 -0400

    added elements to various parameter lists.

--- PSRpy/parfile/config_parfile.py
+++ PSRpy/parfile/config_parfile.py
@@ -25,7 +25,7 @@ parameter_list_DM = ["DM"] +\
 
 parameter_list_DMX = ["DMX"]
 for current_bin in range(n_bins_DMX):
-    for current_parameter in ["DMX", "DMXEP", "DMXR1", "DMXR2", "DMXF1", "DMXF2"]:
+    for current_parameter in ["DMX", "DMX1", "DMXEP", "DMXR1", "DMXR2", "DMXF1", "DMXF2"]:
         parameter_list_DMX += ["{0}_{1}".format(current_parameter, str(current_bin).zfill(4))]
 
 # list of FD parameters.
@@ -35,6 +35,8 @@ parameter_list_FD = ["FD{0}".format(num) for num in range(1, n_derivatives_FD)]
 parameter_list_orbit_Kepler = [
     "A1", "PB", "E", "ECC", "OM", "T0", "EPS1", "EPS2", "TASC"
 ]
+
+parameter_list_orbit_DDK = ["KOM", "KIN"]
 
 parameter_list_orbit_PK = ["M2", "SINI", "GAMMA", "DTHETA", "DR"]
 
@@ -58,13 +60,16 @@ parameter_list_string = ["PSR", "PSRJ", "RAJ", "DECJ", "SOLARN0", "EPHEM", "ECL"
 ]
 
 # list of non-physical parameters with integer values.
-parameter_list_int = ["NTOA", "NITS", "NDDM", "EPHVER", "MODE", "DMDATA"]
+parameter_list_int = ["NTOA", "NITS", "NDDM", "EPHVER", "MODE", "DMDATA", "K96", "PLAN"]
 
 # list of epoch parameters that can have fit flags but are not fit parameters.
 parameter_list_epoch = ["PEPOCH", "POSEPOCH", "DMEPOCH", "START", "FINISH"]
 
-# list of red/white-noise parameters for TOA and DM uncertainties.
-parameter_list_error = ["T2EFAC", "T2EQUAD", "ECORR", "JUMP", "DMEFAC", "DMJUMP"]
+# list of flag-based white noise parameters for TOA and DM uncertainties.
+parameter_list_error = ["T2EFAC", "T2EQUAD", "ECORR", "JUMP", "DMEFAC", "DMJUMP", "DMEQUAD"]
+
+# list of red noise parameters.
+parameter_list_rednoise = ["RNAMP", "RNIDX"]
 
 # list of all parameters defined above.
 parameter_list_full = \
@@ -72,7 +77,8 @@ parameter_list_full = \
     parameter_list_astrometry + parameter_list_spin + parameter_list_DM + \
     parameter_list_DMX + parameter_list_FD + parameter_list_orbit_Kepler +\
     parameter_list_orbit_BTX + parameter_list_epoch + parameter_list_orbit_derivatives +\
-    parameter_list_orbit_PK + parameter_list_error
+    parameter_list_orbit_PK + parameter_list_orbit_DDK + parameter_list_error +\
+    parameter_list_rednoise
 
 # prune above list for duplicates, while preserving order.
 seen = set()
