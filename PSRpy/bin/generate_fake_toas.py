c33d726e4fc27da86fa2d07d1ac8fadd80e9dd58
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2024-07-22 10:22:38 -0400

    added more options

--- PSRpy/bin/generate_fake_toas.py
+++ PSRpy/bin/generate_fake_toas.py
@@ -17,34 +17,51 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--timespan",
-    default=2.0,
-    dest="timespan",
-    type=np.float,
-    help="the timespan of the simulated TOA data set"
-)
-
-parser.add_argument(
-    "--start",
-    default=58515.0,
-    dest="epoch_start",
-    type=np.float,
-    help="the starting epoch in MJD"
+    "--campaigns",
+    default=[],
+    dest="campaigns",
+    nargs="+",
+    type=float,
+    help="the desired RMS residual of simualted data set"
 )
 
 parser.add_argument(
     "--rms",
     default=1.0,
     dest="rms_residual",
-    type=np.float,
+    type=float,
     help="the desired RMS residual of simualted data set"
+)
+
+parser.add_argument(
+    "--scanlength",
+    default=1.,
+    dest="n_hours_per_epoch",
+    type=float,
+    help="scan length per epoch, in hours."
+)
+
+parser.add_argument(
+    "--start",
+    default=58515.0,
+    dest="epoch_start",
+    type=float,
+    help="the starting epoch, in MJD"
+)
+
+parser.add_argument(
+    "--timespan",
+    default=2.0,
+    dest="timespan",
+    type=float,
+    help="the timespan of the simulated TOA data set"
 )
 
 parser.add_argument(
     "--uncertainty",
     default=1.0,
     dest="toa_uncertainty",
-    type=np.float,
+    type=float,
     help="the mean uncertainty of simulated TOAs"
 )
 
@@ -52,7 +69,7 @@ parser.add_argument(
     "--Nchannels",
     default=1,
     dest="n_chan",
-    type=np.int,
+    type=int,
     help="the number of frequency channels to simulate"
 )
 
@@ -60,12 +77,21 @@ parser.add_argument(
     "--Nepochs",
     default=6,
     dest="n_epochs",
-    type=np.int,
+    type=int,
+    help="the number of observing epochs to simulate"
+)
+
+parser.add_argument(
+    "--Nsubints",
+    default=20,
+    dest="n_toas_per_epoch",
+    type=int,
     help="the number of observing epochs to simulate"
 )
 
 # now retrieve data from the command line.
 args = parser.parse_args()
+campaigns = args.campaigns
 input_parfile = args.parfile
 timespan = args.timespan
 epoch_start = args.epoch_start
@@ -73,9 +99,19 @@ rms_residual = args.rms_residual
 toa_uncertainty = args.toa_uncertainty
 n_chan = args.n_chan
 n_epochs = args.n_epochs
+n_hours_per_epoch = args.n_hours_per_epoch
+n_toas_per_epoch = args.n_toas_per_epoch
 
 # derive quantities from command-line values.
 epoch_finish = epoch_start + (365.25 * timespan)
+
+# before simulation, create separate array of timestamps for orbital-campaign obs.
+mjds_campaigns = []
+
+for current_campaign_epoch in campaigns:
+    for ii in range(100):
+        current_offset = ii * (5. / 60. / 24.)
+        mjds_campaigns += [current_campaign_epoch + current_offset]
 
 # now simulate data set.
 print("creating TOA set for {0} epochs...".format(n_epochs))
@@ -85,8 +121,11 @@ sim = simulate_TOAs(
     epoch_start=epoch_start, 
     epoch_finish=epoch_finish, 
     n_channels_per_epoch=n_chan, 
-    n_epochs=n_epochs, 
-    mean_toa_uncertainty=toa_uncertainty, 
+    n_epochs=n_epochs,
+    n_hours_per_epoch=n_hours_per_epoch,
+    n_toas_per_epoch=n_toas_per_epoch,
+    mean_toa_uncertainty=toa_uncertainty,
+    mjds_extra=mjds_campaigns,
     rms_residual=rms_residual, 
     observatory_code='@', 
     jitter_epoch=1, 
